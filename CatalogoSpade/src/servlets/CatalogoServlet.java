4df7d801dcf92cec07b38831b19d9aa3e220e4ac
CasadioAlex <casadioalex04@gmail.com>
2022-05-16 14:05:37 +0200

    modificata la servlet

--- CatalogoSpade/src/servlets/CatalogoServlet.java
+++ CatalogoSpade/src/servlets/CatalogoServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,24 +15,31 @@ import database.DbConnection;
 
 public class CatalogoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String QRY_ARMI_LISTA_TUTTO = "SELECT ar.*,ct.NomeCategoria\n"
+														+ "FROM Arma AS ar,Categoria AS ct\n"
+														+ "WHERE ar.ID_Categoria = ct.ID;";
        
     public CatalogoServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.err.println("doGet Start");
+		boolean testDB;
 		response.setContentType("text/html");
 		PrintWriter stampa = response.getWriter();
-		stampa.println("<p>questa e' una prova</p>");
+		stampa.println("<p>questa e' una prova del get</p>");
+		if(request.getParameter("catalogo") != null && request.getParameter("catalogo").equals("mostraCatalogo")) {
+			DbConnection dbConnection = new DbConnection();
+			testDB = dbConnection.connect(); //TODO: Non stiamo inserendo niente, solo test per connessione con il Database
+			dbConnection.close();
+			RequestDispatcher dispatcher = request.getRequestDispatcher("pages/MostraCatalogo.jsp");
+			dispatcher.forward(request, response);
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter stampa = response.getWriter();
-		stampa.println("<p>questa e' una prova</p>");
-		DbConnection dbConnection = new DbConnection();
-		dbConnection.insert(); //TODO: Non stiamo inserendo niente, solo test per connessione con il Database
-		dbConnection.close();
 	}
 	
 	
